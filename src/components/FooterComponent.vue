e9820cf66a6151ad1cefe41d6462d03c56b9c914
Damir Bubanović <damir.bubanovic@yahoo.com>
2023-02-08 02:23:05 +0100

    Start

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-100 pb-2">
+    <p >&copy; Copyright 2023 <a @click.stop.prevent="snackbar" class="text-blue-lighten-2 font-weight-black text-decoration-none" href="">Damir Bubanovic</a></p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'FooterComponent',
+    methods: {
+      snackbar() {
+        this.$emit('snackbarEmail', true);
+      }
+    }
+  }
+</script>
